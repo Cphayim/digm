820da8e61f4d42ca02e17463bc1c770c20f7f557
Cphayim <i@cphayim.me>
2022-07-31 20:42:00 +0800

    chore: add publish config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (format) => `index${format === 'es' ? '.mjs' : '.cjs'}`,
     },
     rollupOptions: {
-      external: ['@cphayim/digm-shared'],
+      external: ['51superapi', '@cphayim/digm-shared'],
     },
   },
   plugins: [
