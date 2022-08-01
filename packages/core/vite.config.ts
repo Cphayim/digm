c731e9e3e9f94cd89633205104a6f1cdaae744f7
Cphayim <i@cphayim.me>
2022-08-01 14:15:14 +0800

    chore(core): dev build mode

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,36 +2,38 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    sourcemap: true,
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index${format === 'es' ? '.mjs' : '.cjs'}`,
-    },
-    rollupOptions: {
-      external: ['51superapi', '@cphayim/digm-shared'],
-    },
-  },
-  define: {
-    __DEV__: process.env.NODE_ENV === '"development"',
-  },
-  plugins: [
-    dts({
-      rollupTypes: true,
-      copyDtsFiles: false,
-      beforeWriteFile: (filePath, content) => {
-        return { filePath, content }
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      outDir: resolve(__dirname, 'dist'),
+      emptyOutDir: true,
+      sourcemap: true,
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        formats: ['es', 'cjs'],
+        fileName: (format) => `index${format === 'es' ? '.mjs' : '.cjs'}`,
       },
-    }),
-  ],
-  resolve: {
-    dedupe: ['@cphayim/digm-shared'],
-  },
-  optimizeDeps: {
-    exclude: ['@cphayim/digm-shared'],
-  },
+      rollupOptions: {
+        external: ['51superapi', '@cphayim/digm-shared'],
+      },
+    },
+    define: {
+      __DEV__: mode === 'development',
+    },
+    plugins: [
+      dts({
+        rollupTypes: true,
+        copyDtsFiles: false,
+        beforeWriteFile: (filePath, content) => {
+          return { filePath, content }
+        },
+      }),
+    ],
+    resolve: {
+      dedupe: ['@cphayim/digm-shared'],
+    },
+    optimizeDeps: {
+      exclude: ['@cphayim/digm-shared'],
+    },
+  }
 })
