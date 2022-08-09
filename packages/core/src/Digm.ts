d514bff881394dc7657306d37656881f03549a17
ycliu.yongchao <recuritis@gmail.com>
2022-08-09 22:43:47 +0800

    feat(core):implements strategy_map functions and events (#29)
    * feat(core): implements strategy_map functions and events
    
    * fix(core): change _renderer type from any to CloudRendererType

--- packages/core/src/Digm.ts
+++ packages/core/src/Digm.ts
@@ -16,6 +16,7 @@ import {
   ParticleEffect,
   Light,
   Range,
+  StrategyMap,
 } from './features'
 
 export interface FetchRenderUrlOptions {
@@ -59,10 +60,12 @@ export type StartEngineOptions = FetchRenderUrlOptions & RenderPrepareOptions
 
 export type StatusSubscriber = (status: RenderStatus) => void | Promise<void>
 
-export class Digm {
-  private _renderer: any
+export type CloudRendererType = typeof CloudRenderer
 
-  get renderer(): any {
+export class Digm {
+  private _renderer: CloudRendererType
+
+  get renderer(): CloudRendererType {
     this._verifyStatus()
     return this._renderer
   }
@@ -95,6 +98,7 @@ export class Digm {
   public readonly particleEffect: ParticleEffect = new ParticleEffect(this)
   public readonly light: Light = new Light(this)
   public readonly range: Range = new Range(this)
+  public readonly strategyMap: StrategyMap = new StrategyMap(this)
 
   public readonly building: Building = new Building(this)
 
