240618f56727965b9df8fca07ed02d009b90f7a3
Cphayim <i@cphayim.me>
2022-08-10 02:40:43 +0800

    chore(react): add eslint

---
+++ packages/react/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ['@ombro/eslint-config-react-typescript'],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
