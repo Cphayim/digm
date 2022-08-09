ac88c3bf04b7ec235c00659ceefdffa6e69f0193
Cphayim <i@cphayim.me>
2022-08-10 03:27:03 +0800

    chore(shared): eslint-disable any in ensureObject

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,6 +3,7 @@ export const noop = () => void 0
 /**
  * 确保得到一个对象，如果传入的类型是 json 字符串则进行 parse
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function ensureObject<T = any>(obj: unknown): T {
   return typeof obj === 'string' ? JSON.parse(obj) : obj
 }
